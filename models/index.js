fce5ae87bc44bed4941d9a3f721f153ef926dc45
KBradshaw64 <130187631+KBradshaw64@users.noreply.github.com>
2023-07-20 15:32:20 -0700

    finished models and started adding details to routes

--- models/index.js
+++ models/index.js
@@ -5,12 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  // when category gets deleted - deletes the associated product
+  onDelete: 'cascade'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreign_key: 'category_id',
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag,
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag,
+  foreignKey: 'tag_id'
+});
+
 
 module.exports = {
   Product,
